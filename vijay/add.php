14d450eb5a718c3b7c62596d867ace9309829f72
vijayboomika <boomivijay10@gmail.com>
2024-06-21 11:32:05 +0530

    Update add.php

--- vijay/add.php
+++ vijay/add.php
@@ -1,24 +1,15 @@
 <?php
 include("config.php");
 
-
-
 header("Content-type:application/json");
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
 header('Access-Control-Allow-Headers: token, Content-Type');
 
-
-
-
-
-
-
 $json=file_get_contents('php://input');
 $obj=json_decode($json, true);
 
 // echo $name;
-
 
 $name=$obj['name'];
 $mobile=$obj['mobile'];
